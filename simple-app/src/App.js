70ad977025a44050a4536ec366ea1a0b1b46ee28
MarceloAvanzi <marcelodaniel@hotmail.com>
2024-01-09 20:59:25 -0300

    fixing folders

---
+++ simple-app/src/App.js
@@ -0,0 +1,22 @@
+// src/App.js
+import React, { useState } from 'react';
+import giphyService from './services/giphy.service.js';
+
+const App = () => {
+  const [gifs, setGifs] = useState([]);
+  const handleSearch = async (query) => setGifs(await giphyService.getGifs(query));
+
+  return (
+    <div>
+      <h1>GIF Search App</h1>
+      <input type="text" onChange={(e) => handleSearch(e.target.value)} />
+      <div>
+        {gifs.map((gif) => (
+          <img key={gif.id} src={gif.images.fixed_height.url} alt={gif.title} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
